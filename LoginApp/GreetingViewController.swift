c41eee44cf6f28158b8ce0ab3a99154d9c5b452c
AndreySandler <mrdreem13@gmail.com>
2022-10-01 01:30:30 +0300

    Setting the logic for passing data from the first screen to the second

--- LoginApp/GreetingViewController.swift
+++ LoginApp/GreetingViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class GreetingViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet var greetingLabel: UILabel!
+    
+    // MARK: - Public properties
+    var greeting: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        greetingLabel.text = "Welcome, " + greeting
     }
+    
 }
--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    // MARK: - IBOutlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        
+        self.view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let greetingVC = segue.destination as? GreetingViewController else { return }
+        
+        greetingVC.greeting = userNameTextField.text
+    }
+    
+    // MARK: - IBAction
+    @IBAction func forgotButtonDidTapped(_ sender: UIButton) {
+        switch sender {
+        case forgotUsernameButton:
+            showAlert(withTitle: "Oops", andMessage: "Your name is User")
+        default:
+            showAlert(withTitle: "Oops", andMessage: "Your pass is Password")
+        }
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
         
     }
+}
 
-
+// MARK: - UIAlertController
+extension LoginViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
